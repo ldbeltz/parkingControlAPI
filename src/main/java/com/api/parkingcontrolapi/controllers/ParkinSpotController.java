f0e38f774e959aa2e70338a7d061457792eb8652
Humberto <humbertocardoso.lima@sead.se.gov.br>
2023-03-14 16:36:14 -0300

    Refactor: clear imports

--- src/main/java/com/api/parkingcontrolapi/controllers/ParkinSpotController.java
+++ src/main/java/com/api/parkingcontrolapi/controllers/ParkinSpotController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
